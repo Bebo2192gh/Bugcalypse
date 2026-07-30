be14a1441558b9b20c503a880a48c4e365f87db1
bebinho <pedrorosalazzarin2605@gmail.com>
2026-07-30 17:04:29 -0300

    testing commiting

--- main.lua
+++ main.lua
@@ -6,6 +6,12 @@ function love.load()
     love.window.setTitle("Meu Jogo")
 end
 
+function love.update(dt)
+    if screen == "menu" then
+        Menu.update(dt)
+    end
+end
+
 function love.draw()
     if screen == "menu" then
         Menu.draw()
