65a64c1de76a6acc20b2348a6d2fa8c5f1218f23
bebinho <pedrorosalazzarin2605@gmail.com>
2026-07-31 03:08:59 -0300

    Ajusta a árvore, adiciona a luz vermelha e adiciona o fade de opacidade ao abrir o jogo.

--- main.lua
+++ main.lua
@@ -2,13 +2,16 @@ local Menu = require("src.menu")
 local Game = require("src.game")
 
 screen = "menu"
+fade = 1
 
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
     love.window.setTitle("Meu Jogo")
+
     title = love.graphics.newImage("assets/images/title.png")
     playButton = love.graphics.newImage("assets/sprites/playbutton.png")
     configButton = love.graphics.newImage("assets/sprites/configbutton.png")
+    redlight = love.graphics.newImage("assets/images/redlight.png")
 end
 
 function love.update(dt)
@@ -17,20 +20,28 @@ function love.update(dt)
     elseif screen == "game" then
         Game.update(dt)
     end
+
+    fade = math.max(fade - dt * 0.5, 0)
 end
 
 function love.mousepressed(x, y, button)
     if button == 1 then
         if x >= 20 and x <= 220 and y >= 200 and y <= 260 then
             screen = "game"
+            fade = 1
         end
     end
 end
 
 function love.draw()
+
     if screen == "menu" then
         Menu.draw()
     elseif screen == "game" then
         Game.draw()
     end
+
+    love.graphics.setColor(0, 0, 0, fade)
+    love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+    love.graphics.setColor(1, 1, 1, 1)
 end
--- src/menu.lua
+++ src/menu.lua
@@ -5,6 +5,7 @@ function Menu.draw()
     love.graphics.draw(playButton, 1280 / 2 - playButton:getWidth() / 2, 250)
     love.graphics.draw(configButton, 1280 / 2 - configButton:getWidth() / 2, 360)
 
+    love.graphics.draw(redlight, 1280 / 2 - redlight:getWidth() / 2, 80)
     love.graphics.draw(title, 1280 / 2 - title:getWidth() / 2, 100)
     love.graphics.print("made by:ML studios", 1035, 690, 0, 2, 2)
     love.graphics.print("pré-alpha v1.0", 1100, 665, 0, 2, 2)
