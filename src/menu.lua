aaca4afb1780e835884c06f73a7d6d8e6d49ff26
bebinho <pedrorosalazzarin2605@gmail.com>
2026-07-31 00:39:02 -0300

    Muda o game.txt para a .src

--- src/menu.lua
+++ src/menu.lua
@@ -6,7 +6,7 @@ function Menu.draw()
     love.graphics.draw(configButton, 1280 / 2 - configButton:getWidth() / 2, 360)
 
     love.graphics.draw(title, 1280 / 2 - title:getWidth() / 2, 100)
-    love.graphics.print("made by:ML studios", 1035, 690, 0, 2, 2)
+    love.graphics.print("ML studios 2026", 1075, 690, 0, 2, 2)
     love.graphics.print("pré-alpha v1.0", 1100, 665, 0, 2, 2)
 end
 
